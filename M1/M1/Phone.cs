6c7d2e183f21f43d0946c4cd06ccca456d436dbd
Niedritis <niedritis@inbox.lv>
2022-04-24 21:04:18 +0300

    fff

---
+++ M1/M1/Phone.cs
@@ -0,0 +1,20 @@
+﻿namespace M1
+{
+    internal class Phone
+    {
+        public double Size { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public void Call()
+        {
+            Console.WriteLine("Calling to Artis");
+        }
+
+        public void Sms()
+        {
+            Console.WriteLine("SMS sent to Artis");
+        }
+
+    }
+}
---
+++ M1/M1/Program.cs
@@ -0,0 +1,22 @@
+﻿using M1;
+
+Console.WriteLine("1.uzdevums");
+Console.WriteLine("===================");
+
+Phone phone1 = new Phone();
+
+
+
+phone1.Size = 15;
+phone1.Brand = "nokia";
+phone1.Model = "galaxy";
+
+string phonename = phone1.Brand;
+
+string phonemodel = phone1.Model;
+
+Console.WriteLine($"apsveicu jauns telefons {phonename} modelis {phonemodel}");
+
+phone1.Call();
+
+phone1.Sms();
