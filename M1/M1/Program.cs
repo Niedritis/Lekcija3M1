b66466f657ef0284093f88455908d230e3e77c44
Niedritis <niedritis@inbox.lv>
2022-04-25 23:37:05 +0300

    Gatavs

--- M1/M1/Program.cs
+++ M1/M1/Program.cs
@@ -4,17 +4,21 @@
 
 Phone phone1 = new Phone();
 
+Console.WriteLine("Lūdzu, ievadi Telefona zīmolu");
+string PhoneBrand = Console.ReadLine();
+phone1.Brand = PhoneBrand;
+
+Console.WriteLine("Lūdzu, ievadi Telefona modeli");
+string PhoneModel = Console.ReadLine();
+phone1.Model = PhoneModel;
+
+Console.WriteLine("Lūdzu, ievadi Telefona garumu");
+string PhoneLenght = Console.ReadLine();
+phone1.Size = int.Parse(PhoneLenght);
 
 
-phone1.Size = 15;
-phone1.Brand = "nokia";
-phone1.Model = "galaxy";
 
-string phonename = phone1.Brand;
-
-string phonemodel = phone1.Model;
-
-Console.WriteLine($"apsveicu jauns telefons {phonename} modelis {phonemodel}");
+Console.WriteLine($"Super aparats {phone1.Brand} {phone1.Model} ir {phone1.Size} cm garš");
 
 phone1.Call();
 
